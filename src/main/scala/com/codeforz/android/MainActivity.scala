65735cd6cb35cba582a4643cdfc0c168a8de6565
julio <julio@ticotico.local>
2013-08-08 22:21:37 +1000

    initial commit

---
+++ src/main/scala/com/codeforz/android/MainActivity.scala
@@ -0,0 +1,15 @@
+package com.codeforz.android
+
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * 
+ */
+class MainActivity extends Activity{
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    //TODO: app launch
+  }
+}
